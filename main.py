7fe4f560637893193bf6fc54ac3254199e565cb1
aberlanga25 <natacion3>
2019-09-03 15:06:00 -0600

    Fill Tab1

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
 import sys
 from PyQt5.QtWidgets import *
-from tab1 import fillTab1
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot, Qt
+import tab1
 
 class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'BEAT: Behavioral Extraction and Analysis Tool'
-        self.left = 0
-        self.top = 0
+        self.title = 'BEAT: Beahavioral Extraction Tool'
+        self.left = 200
+        self.top = 500
         self.width = 800
-        self.height = 600
+        self.height = 800
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.table_widget = Tabs(self)
+        self.table_widget = MyTableWidget(self)
         self.setCentralWidget(self.table_widget)
 
         self.show()
 
-class Tabs(QWidget):
+class MyTableWidget(QWidget):
 
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
 
-        # Initialize tab screen
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
         self.tab2 = QWidget()
         self.tab3 = QWidget()
         self.tab4 = QWidget()
         self.tab5 = QWidget()
-        self.tabs.resize(300, 200)
+        self.tabs.resize(800, 800)
 
-        # Add tabs
-        self.tabs.addTab(self.tab1, "Tab 1")
-        self.tabs.addTab(self.tab2, "Tab 2")
-        self.tabs.addTab(self.tab3, "Tab 3")
-        self.tabs.addTab(self.tab4, "Tab 4")
-        self.tabs.addTab(self.tab5, "Tab 5")
+        self.tabs.addTab(self.tab1, "Project")
+        self.tabs.addTab(self.tab2, "Analysis")
+        self.tabs.addTab(self.tab3, "Plugin Management")
+        self.tabs.addTab(self.tab4, "Points of Interest")
+        self.tabs.addTab(self.tab5, "Documentation")
 
-        self.tab1.setLayout(fillTab1(self.tab1))
 
-        # Add tabs to widget
+        self.tab1.setLayout(tab1.fillTab1(self.tab1, parent))
+
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
+
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
 
 
 if __name__ == '__main__':
--- tab1.py
+++ tab1.py
@@ -1,7 +1,47 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-def fillTab1(widget):
-    widget.layout = QVBoxLayout()
-    pushbutton = QPushButton("PyQt5 button")
-    widget.layout.addWidget(pushbutton)
+def fillTab1(widget, parent):
+    widget.layout = QHBoxLayout()
+
+    frameProj = QFrame()
+    frameProj.setFrameShape(QFrame.StyledPanel)
+    frameProj.setLineWidth(0.6)
+    frameProj.setMaximumWidth(parent.width / 5)
+    frameDet = QFrame()
+    frameDet.setFrameShape(QFrame.StyledPanel)
+    frameDet.setLineWidth(0.6)
+
+    titleFont = QFont("Times", 15, QFont.Bold)
+
+    #Project View
+    lblProjView = QLabel("Project View", frameProj)
+    lblProjView.move((frameProj.width()-lblProjView.width())/3, 5)
+    lblProjView.setFont(titleFont)
+    txtSearch = QLineEdit(frameProj)
+    txtSearch.setClearButtonEnabled(True)
+    txtSearch.addAction(QIcon("resources/search.png"), QLineEdit.LeadingPosition)
+    txtSearch.setPlaceholderText("Search...")
+    txtSearch.move(15, lblProjView.height()+10)
+    listProject = QListWidget(frameProj)
+    listProject.move(5, lblProjView.height()+50)
+    listProject.setMaximumWidth(frameProj.width()-10)
+    listProject.setMinimumHeight(frameProj.height()+150)
+    listProject.insertItem(0, "Project A")
+    listProject.insertItem(1, "Project B")
+    listProject.insertItem(2, "Project C")
+    listProject.insertItem(3, "Project D")
+    listProject.setStyleSheet('''QWidget {border:none;}''')
+    btnNewProj = QPushButton("New", frameProj)
+    btnNewProj.move(70, frameProj.height()+220)
+
+    #Project Detailed View
+    lblProjDet = QLabel("Detailed Project View", frameDet)
+    lblProjDet.move((frameDet.width()-lblProjDet.width())/3, 5)
+    lblProjDet.setFont(titleFont)
+
+
+    widget.layout.addWidget(frameProj)
+    widget.layout.addWidget(frameDet)
     return widget.layout
