00b2184d179cc9d9e926762b83a8dd5156942cb9
Sergio Yanez <seryan.1115@gmail.com>
2019-11-25 12:08:34 -0700

    fixed relative path errors in documentation tab V. 5.1

--- src/docx2html.py
+++ src/docx2html.py
@@ -3,7 +3,7 @@ import sys, os, re
 if len(sys.argv) != 2:
     print('wrong format: expected "<filename>" do NOT include file type.')
 elif os.path.isfile('{}.docx'.format(sys.argv[1])):
-    os.system('pandoc -f docx -t html {0}.docx -o resources/documentation/{0}.html \
-    --extract-media=resources/documentation/{0}'.format(sys.argv[1]))
+    os.system('pandoc -f docx -t html {0}.docx -o resources/docs/{0}.html \
+    --extract-media=resources/docs/{0}'.format(sys.argv[1]))
 else:
     print('"{}.docx" not found on the current directory.'.format(sys.argv[1]))
